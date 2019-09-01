d82e7bcddc7d8f492bbbeece1f717b6ad3399a63
chrisedell <chrise.delladonna@gmail.com>
2019-09-01 18:56:00 +0000

    Added comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
  
-  has_many :places         
+  has_many :places
+  has_many :comments        
 end
