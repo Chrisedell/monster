a7a5243a96cc0b8fa852dda7121faacf3fbf8cf0
chrisedell <chrise.delladonna@gmail.com>
2019-09-16 18:36:47 +0000

    39 Made changes to migration for photos

--- db/migrate/20190903202253_create_photos.rb
+++ db/migrate/20190903202253_create_photos.rb
@@ -4,9 +4,10 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
       t.text :caption
       t.integer :place_id
       t.timestamps
-    end
   end
-
+  
   add_index :photos, [:user_id, :place_id]
   add_index :photos, :place_id
+   
+  end
 end
