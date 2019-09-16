915e4ed93657db45a3e370a692b6baa64ed78282
chrisedell <chrise.delladonna@gmail.com>
2019-09-16 18:57:18 +0000

    39 Moved addindex to adduser migration

--- db/migrate/20190908225116_add_user_id_to_photos.rb
+++ db/migrate/20190908225116_add_user_id_to_photos.rb
@@ -2,4 +2,9 @@ class AddUserIdToPhotos < ActiveRecord::Migration[5.2]
   def change
     add_column :photos, :user_id, :integer
   end
+    
+  add_index :photos, [:user_id, :place_id]
+  add_index :photos, :place_id
+   
+  end
 end
